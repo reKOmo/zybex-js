69e57bfb0a356cfe4013c0d15614cb9436666c7c
koMOre <zadanka.i.testy@protonmail.com>
2022-10-17 22:53:03 +0200

    fix config for netlify

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
 import typescript from '@rollup/plugin-typescript';
 const html = require('@rollup/plugin-html');
+import copy from 'rollup-plugin-copy'
 
 export default {
   input: 'src/index.ts',
   output: {
-    dir: 'build',
+    dir: 'dist',
     format: 'es'
   },
-  plugins: [typescript({ tsconfig: "./tsconfig.json" }), html({ title: "Zybex - projekt końcowy", attributes: { script: { defer: true } } })]
+  plugins: [typescript({ tsconfig: "./tsconfig.json" }), html({ title: "Zybex - projekt końcowy", attributes: { script: { defer: true } } }), copy({ targets: [{ src: "assets", dest: "dist" }] })]
 };
